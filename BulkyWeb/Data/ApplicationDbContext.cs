b58d1792eb918d226750179c5270416685533f85
Dzx <zexuding@outlook.com>
2024-02-10 12:30:02 +0800

    Database

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Category> Categories { get; set; }
+    }
+}
