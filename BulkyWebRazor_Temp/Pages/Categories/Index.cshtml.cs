8c936d6f61af12742208c82d8390c67a1f688964
Dzx <dzxdzx12345@gmail.com>
2024-02-11 20:14:09 +0800

    Add Index

---
+++ BulkyWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    public class IndexModel(ApplicationDbContext db) : PageModel
+    {
+        public List<Category>? CategoryList { get; set; }
+        public void OnGet()
+        {
+            CategoryList = [.. db.Categories];
+        }
+    }
+}
