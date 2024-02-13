1d22ebacc39076eaad8af47b0c60877f7be2a55a
Dzx <dzxdzx12345@gmail.com>
2024-02-13 14:22:37 +0800

    Add category repository

---
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository;
+
+public class CategoryRepository(ApplicationDbContext _db) : Repository<Category>(_db), ICategoryRepository
+{
+    private readonly ApplicationDbContext _db = _db;
+
+    public void Save()
+    {
+        _db.SaveChanges();
+    }
+
+    public void Update(Category obj)
+    {
+        _db.Update(obj);
+    }
+}
