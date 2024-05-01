cb6a833d38d4aa4ac5225cd065161e40e6e02897
Dzx <dzxdzx12345@gmail.com>
2024-05-02 00:10:47 +0800

    Extend default user model

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -9,7 +9,7 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 {
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
-
+    public DbSet<IdentityUser> ApplicationUsers { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
