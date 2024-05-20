b66c97701805be0fcc66c1aa870550c4ba1dcd8d
Dzx <dzxdzx12345@gmail.com>
2024-05-20 18:17:49 +0800

    Model code refactor

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -6,16 +6,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BulkyBook.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        [Required]
-        public string Name { get; set; }
+namespace BulkyBook.Models;
 
-        public string? StreetAddress { get; set; }
-        public string? City { get; set; }
-        public string? State { get; set; }
-        public string? PostalCode { get; set; }
-    }
+public class ApplicationUser : IdentityUser
+{
+    [Required]
+    public string Name { get; set; }
+
+    public string? StreetAddress { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? PostalCode { get; set; }
 }
